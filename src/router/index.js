0b9a44f7edfa482bc06adb09e58750cbf4d679da
Chris Gartsen <chris@gartsen.nl>
2021-12-23 21:32:37 +0100

    Add route guards

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,50 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
+import store from '@/store'
 
 const routes = [
   {
-    path: '/', component: Home
+    name: 'Home',
+    path: '/',
+    component: Home
   },
   {
-    path: '/about', component: About
+    name: 'About',
+    path: '/about',
+    component: About
   },
   {
-    path: '/manage', component: Manage
+    name: 'Manage',
+    path: '/manage-music',
+    component: Manage,
+    alias: '/manage',
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'Home' }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
+})
+
+router.beforeEach((to, from, next) => {
+  
+  if (!to.matched.some(record => record.meta.requiresAuth)) {
+    next()
+    return
+  }
+
+  if (store.state.userLoggedIn) {
+    next()
+  } else {
+    next({ name: 'Home' })
+  }
 })
 
 export default router
