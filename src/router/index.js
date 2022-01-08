ae5854be120daac384cf5f982b50653bc23a1bd5
Chris Gartsen <chris@gartsen.nl>
2022-01-08 20:38:02 +0100

    Add song page and route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Manage from '@/views/Manage.vue'
+import Song from '@/views/Song.vue'
+
 import store from '@/store'
 
 const routes = [
@@ -21,6 +23,11 @@ const routes = [
     component: Manage,
     alias: '/manage',
     meta: { requiresAuth: true },
+  },
+  {
+    name: 'Song',
+    path: '/song/:id',
+    component: Song
   },
   {
     path: '/:catchAll(.*)*',
---
+++ src/views/Song.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>{{ $route.params.id }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Song',
+
+}
+</script>
