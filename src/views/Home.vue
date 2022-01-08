30025c93716580780b99b2ef9484786e30fffdb0
Chris Gartsen <chris@gartsen.nl>
2022-01-08 20:07:47 +0100

    Add infinite scrolling

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -51,16 +51,65 @@ export default {
   },
   data() {
     return {
-      songs: []
+      songs: [],
+      maxPerPage:20,
+      pendingRequests: false
+    }
+  },
+  methods: {
+    async getSongs() {
+      if(this.pendingRequests) {
+        return 
+      }
+
+      this.pendingRequests = true
+
+      let snapshots
+
+      if (this.songs.length) {
+        const lastDoc = await songsCollection
+          .doc(this.songs[this.songs.length - 1].docID)
+          .get()
+
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .startAfter(lastDoc)
+          .limit(this.maxPerPage)
+          .get()
+      } else {
+        snapshots = await songsCollection
+          .orderBy('modified_name')
+          .limit(this.maxPerPage)
+          .get()
+      }
+
+      snapshots.forEach((document) => {
+        this.songs.push({ docID: document.id, ...document.data() })
+      })
+
+      this.pendingRequests = false
+    },
+    handleScroll() {
+      const { scrollTop, offsetHeight } = document.documentElement
+      const { innerHeight } = window
+
+      const bottomOfWindow =
+        Math.round(scrollTop) + innerHeight === offsetHeight
+
+      if (bottomOfWindow) {
+        console.log("Loading songs")
+        this.getSongs()
+      }
     }
   },
 
   async created() {
-    const snapshots = await songsCollection.get()
+    this.getSongs()
 
-    snapshots.forEach((document) => {
-      this.songs.push({ docID: document.id, ...document.data() })
-    })
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
