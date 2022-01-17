7762ff3496d97e9f76f85b0643be33dfc019b280
Chris Gartsen <chris@gartsen.nl>
2022-01-17 19:26:53 +0100

    Change locale

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -36,6 +36,13 @@
             </li>
           </template>
         </ul>
+        <ul class="flex flex-row mt-1 ml-auto">
+          <li>
+            <a href="#" class="px-2 text-white" @click.prevent="changeLocale">
+              {{ currentLocale }}
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -47,7 +54,10 @@ import { mapMutations, mapState } from 'vuex'
 export default {
   name: 'Header',
   computed: {
-    ...mapState(['userLoggedIn'])
+    ...mapState(['userLoggedIn']),
+    currentLocale() {
+      return this.$i18n.locale === 'fr' ? 'Français' : 'English'
+    }
   },
   methods: {
     ...mapMutations(['toggleAuthModal']),
@@ -57,6 +67,9 @@ export default {
       if (this.$route.meta.requiresAuth) {
         this.$router.push({ name: 'Home' })
       }
+    },
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale === 'fr' ? 'en' : 'fr'
     }
   }
 }
